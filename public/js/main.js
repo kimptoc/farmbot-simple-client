93df6deede0e60ffac42ad4274fabf2cde5a8bfe
Chris Kimpton <chris@kimptoc.net>
2017-07-07 00:18:34 +0100

    use localstorage to save email/password. support for toggling arduino LED

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
+    function setFieldFromLocalStorage(fieldRef, storageKey){
+        var val = localStorage.getItem(storageKey);
+        if (val) {
+            $(fieldRef).val(val);
+        }
+        return val;
+    }
+    var email = setFieldFromLocalStorage('#email','fb.email');
+    var passwd = setFieldFromLocalStorage('#password','fb.password');
+
+    if (email && passwd) {
+        doLogin();
+    }
+
     function buildButton(name, id){
         return '<div><button id="sequence-'+id+'" sequence-id="'+id+'" class="trigger-sequence">'+name+'</button></div>';
     }
@@ -15,19 +29,28 @@ $(function() {
         });
     }
 
+    function doLogin(){
+        localStorage.setItem('fb.email',$('#email').val());
+        localStorage.setItem('fb.password',$('#password').val());
+
+        $('#div1').html('working...');
+          botui = new FarmbotUI()
+          botui.login($('#email').val(),$('#password').val(), function(response) {
+              localStorage.setItem('fb.token',response.token);
+              $('#div1').html(JSON.stringify(response.raw_response));
+              bot = new Farmbot.Farmbot({token:localStorage.getItem('fb.token'), secure: true});
+              bot.connect();
+          });
+    }
+
     $( "#login" ).click(function() {
-      $('#div1').html('working...');
-        botui = new FarmbotUI()
-        botui.login($('#email').val(),$('#password').val(), function(response) {
-                   $('#token').html(response.token);
-                   $('#div1').html(JSON.stringify(response.raw_response));
-        });
+        doLogin();
       return false;
     });
 
     $( "#sequences" ).click(function() {
       $('#div1').html('working...');
-        botui.sequences($('#token').html(), function(response) {
+        botui.sequences(localStorage.getItem('fb.token'), function(response) {
             $.each(response.sequences, function(index, sequence){
 //                $('#sequence_list').append('<div>'+sequence.name+'/'+sequence.id+'</div>')
                 $('#sequence_list').append(buildButton(sequence.name,sequence.id))
@@ -40,17 +63,17 @@ $(function() {
         });
       return false;
     });
+    $( "#arduino_led" ).click(function() {
+        bot.togglePin({pin_number: 13});
+    });
     $( "#device" ).click(function() {
       $('#div1').html('working...');
-      bot = new Farmbot.Farmbot({token:$('#token').html(), secure: true});
 
-      bot.connect();
-
-      $('#div1').html('connected! Status:'+bot.hardware.informational_settings.sync_status);
+//      $('#div1').html('connected! Status:'+bot.hardware.informational_settings.sync_status);
 
       bot.on("*", function(data, eventName) {
           console.log("I just got an" + eventName + " event!");
-          console.log("This is the payload: " + data);
+          console.log("This is the payload: " + JSON.stringify(data));
         })
 
     });
